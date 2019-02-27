ebc1b5fe03299cc5db93044ac2a84295d27d464c
Meredith <dinsdale.meredith@gmail.com>
2019-02-26 18:48:15 -0800

    a few updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,15 +3,15 @@ $(document).ready(function() {
 	//Global Variables
 	var currentQ;		//Question currently being asked
 
-	var correctAnswer;	// ------------------
+	var correctAnswer;	
 	var wrongAnswer;	//Score Count
-	var unanswered;		// ------------------
+	var unanswered;		
 
 	var answered; 		//Used as Boolean
 	
-						//=============
+						
 	var seconds;		//TIMER
-	var time;			//=============
+	var time;			
 	
 	var userChoice;		// Stores user Input
 
@@ -27,7 +27,6 @@ $(document).ready(function() {
 			question: "How big can adult racoons get?",
 			choices: ["20 lbs", "30 lbs", "40 lbs", "50 lbs", "60 lbs"],
 			correct: 4,
-			image: "",
 			answerText: "Adult male raccoons can be as large as 60 lbs! Wouldn't want to run into one of those in your bathrobe at 2 am.",
 		},
 
@@ -35,7 +34,6 @@ $(document).ready(function() {
 			question: "What is the average lifespan of wild raccoons?",
 			choices: ["2.5 years", "5.7 years", "7 years", "10 years", "40 years"],
 			correct: 0,
-			image: "",
 			answerText: "Wild racoons only live about 2.5 years on average. Ain't no rest for the wicked!",
 		},
 
@@ -43,7 +41,6 @@ $(document).ready(function() {
 			question: "What is unique about a racoon's tail?",
 			choices: ["It can be used to hold onto things", "It has black rings up and down it", "It is always twice the length of the tail owner's body", "It can be used as a weapon to whip predators", "It has barbs mixed in with the fur, similar to a porcupine or hedgehog"],
 			correct: 1,
-			image: "",
 			answerText: "Racoon tails have black rings going up and down their tails. It's pretty much for aesthetic purposes only.",
 		},
 
@@ -51,33 +48,29 @@ $(document).ready(function() {
 			question: "What are raccoon young called?",
 			choices: ["Pups", "Budgies", "Kittens", "Cubs", "Tykes"],
 			correct: 2,
-			image: "",
 			answerText: "Baby raccoons are called kittens! Aww...",
 		},
 		{
 			question: "How long can captive raccoons live?",
 			choices: ["4 years", "7 years", "12 years", "16 years", "20 years"],
 			correct: 4,
-			image: "",
 			answerText: 'Raccoons living in captivity can live for up to 20 years. Secret to long life: don\'t eat trash.',
 		},
 		{
 			question: "How many raccoons does it take to screw in a lightbulb?",
 			choices: ["1", "2", "3", "4", "5"],
 			correct: 3,
-			image: "",
 			answerText: 'Four. One to smash the old one, one to smash the new one, one to hold the stool, and one to chew the wires because it\'s too bright in here anyways."'
 		},
 		{
 			question: "What do raccoons not eat?",
 			choices: ["Shellfish", "Red meat", "Carbs", "Gluten", "Pine needles"],
 			correct: 4,
-			image: "",
 			answerText: "Raccoons don't eat pine needles."
 		},
 	];
 
-	// Hides questions when game starts up
+	// Hides questions when game starts
 	$("#gameArea").hide();
 
 	// Hides the start button once it's clicked
@@ -164,18 +157,13 @@ $(document).ready(function() {
 		var rightAnswerIndex = triviaQuestions[currentQ].correct;
 		console.log(rightAnswerText);
 		console.log(rightAnswerIndex);
-		//GIF IMG
-		var gifLink = triviaQuestions[currentQ].image;
-		var Giffy = $("<img>");
-		Giffy.attr("Src", gifLink);
-		Giffy.addClass("gifImg");
-		$("#gif").html(Giffy);
-		// GIF TEXT
-		var gifText = triviaQuestions[currentQ].answerText;
+		
+		// ANSWER TEXT
+		var answer = triviaQuestions[currentQ].answerText;
 			newCap = $("<div>");
-			newCap.html(gifText);
+			newCap.html(answer);
 			newCap.addClass("gifCap");
-			$("#gifText").html(newCap);
+			$("#answerText").html(newCap);
 
 
 		// DISPLAYS AND COUNTS USER ANSWERS/ UnANSWERS
@@ -194,12 +182,12 @@ $(document).ready(function() {
 			answered = true;
 		}
 
-		//Last Answer Reveal Timer
+		//TIMER FOR REVEALED ANSWER
 		if (currentQ === (triviaQuestions.length-1)) {
-			setTimeout(results, 10000);
+			setTimeout(results, 7000);
 		} else {
 			currentQ++;
-			setTimeout(questions, 10000);
+			setTimeout(questions, 7000);
 		}
 
 	}
